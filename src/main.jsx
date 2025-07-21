2ee7555750a2e9b05005b59119aad2d438d53847
shivam.code247@gmial.com <shivam.code247@gmail.com>
2025-07-21 12:18:52 +0530

    day01

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'  
+
+import {a,b} from './App.jsx'
+
+console.log(a,b);
+
+
+createRoot(document.getElementById('root')).render(<App />)
